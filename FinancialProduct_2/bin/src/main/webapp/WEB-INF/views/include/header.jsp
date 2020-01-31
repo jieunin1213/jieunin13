5b04bcca13b9eb29c087d944e4c9010673cca993
jieunin1213 <60494400+jieunin1213@users.noreply.github.com>
2020-01-31 23:15:20 +0900

    Add files via upload

---
+++ FinancialProduct_2/bin/src/main/webapp/WEB-INF/views/include/header.jsp
@@ -0,0 +1,50 @@
+
+
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+    
+	<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+	
+		<a class="navbar-brand mr-auto mr-lg-0" href="<c:url value="/"/>">Finance</a>
+		
+		
+		<button class="navbar-toggler p-0 border-0" type="button"
+			data-toggle="offcanvas">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="navbar-collapse offcanvas-collapse"
+			id="navbarsExampleDefault">
+			<ul class="navbar-nav mr-auto">
+				<%-- <li class="nav-item "><a class="nav-link" href="#">HOME
+						<span class="sr-only">(current)</span>
+				</a></li>
+				<li class="nav-item"><a class="nav-link" href="<c:url value="/member/regist"/>">회원가입</a>	</li>
+				<c:if test="${loginInfo eq null}">
+				<li class="nav-item"><a class="nav-link" href="<c:url value="/member/login"/>">회원로그인</a></li>
+				</c:if>
+				<c:if test="${loginInfo ne null}">
+				<li class="nav-item"><a class="nav-link" href="<c:url value="/member/logout"/>">회원로그아웃</a></li>
+				</c:if>
+				<li class="nav-item"><a class="nav-link" href="<c:url value="/member/list"/>">회원리스트</a></li> --%>
+				<li class="nav-item dropdown"><a
+					class="nav-link dropdown-toggle" href="https://example.com"
+					id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+					aria-expanded="false">금융상품</a>
+					<div class="dropdown-menu" aria-labelledby="dropdown01">
+						<a class="dropdown-item" href="<c:url value="/function/search"/>">상품1</a> 
+						<a class="dropdown-item" href="<c:url value="/function/search"/>">상품2</a>
+					</div>
+				</li>
+			</ul>
+
+			<!-- <form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="text" placeholder="Search"
+					aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form> -->
+		</div>
+	</nav>
+	
---
+++ FinancialProduct_2/src/main/java/com/kite/mm/Financecontroller/FinanceController5.java
@@ -0,0 +1,128 @@
+package com.kite.mm.Financecontroller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class FinanceController5 {
+
+
+	
+	@CrossOrigin
+	@RequestMapping("/function/finance5")
+	public String getInfo(Model model) throws UnsupportedEncodingException {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String key = "7ca1f03b6f18f09837b676a3f0f2370c";
+		String dcodeKey = URLDecoder.decode(key, "utf-8");
+		
+		String url = "http://finlife.fss.or.kr/finlifeapi/rentHouseLoanProductsSearch.xml"
+				+ "?auth="+ dcodeKey
+				+ "&topFinGrpNo=050000"
+				+ "&pageNo=1";
+
+		
+		String result = restTemplate.getForObject(url, String.class);
+		
+		model.addAttribute("result5", result);
+
+		return "function/finance5";		
+	}
+	
+}
+
+
+//package com.kite.mm.controller;
+//
+//import java.io.UnsupportedEncodingException;
+//import java.net.URLDecoder;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Controller
+//public class DictController {
+//
+//	@CrossOrigin
+//	@RequestMapping("/function/dict")
+//	public String getInfo(
+//			@RequestParam ("q") String keyword,
+//			Model model) throws UnsupportedEncodingException {
+//		
+//		RestTemplate restTemplate = new RestTemplate();
+//		String key = "7EDD1EDA2E0D739DA7565168C0C5262E";
+//		String dcodeKey = URLDecoder.decode(key, "utf-8");
+//		
+//		String url = "https://opendict.korean.go.kr/api/search"
+//				+ "?certkey_no=1209"
+//				+ "&key=" + dcodeKey
+//				+ "&target_type=search"
+//				+ "&part=word"
+//				+ "&q=" + keyword
+//				+ "&sort=dict"
+//				+ "&start=1"
+//				+ "&num=10";
+//		
+//		String result = restTemplate.getForObject(url, String.class);
+//		
+//		model.addAttribute("result", result);
+//
+//		return "function/dict";		
+//	}
+//	
+//}
+
+//package com.kite.mm.controller;
+//
+//import java.io.UnsupportedEncodingException;
+//import java.net.URLDecoder;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Controller
+//public class DictController {
+//
+//	@CrossOrigin
+//	@RequestMapping("/function/dict")
+//	public String getInfo(
+//			@RequestParam ("q") String keyword,
+//			Model model) throws UnsupportedEncodingException {
+//		
+//		RestTemplate restTemplate = new RestTemplate();
+//		String key = "7EDD1EDA2E0D739DA7565168C0C5262E";
+//		String dcodeKey = URLDecoder.decode(key, "utf-8");
+//		
+//		String url = "https://opendict.korean.go.kr/api/search"
+//				+ "?certkey_no=1209"
+//				+ "&key=" + dcodeKey
+//				+ "&target_type=search"
+//				+ "&part=word"
+//				+ "&q=" + keyword
+//				+ "&sort=dict"
+//				+ "&start=1"
+//				+ "&num=10";
+//		
+//		String result = restTemplate.getForObject(url, String.class);
+//		
+//		model.addAttribute("result", result);
+//
+//		return "function/dict";		
+//	}
+//	
+//}
